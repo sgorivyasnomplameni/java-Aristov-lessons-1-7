c77345d487b82d7940d409f2b247ec951b2ce9b3
sgorivyasnomplameni <gosurasu@gmail.com>
2024-09-15 19:41:31 +0300

    Ввод и вывод данных

---
+++ src/lesson_4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class lesson_4 {
+    public static void main(String[] args) {
+        System.out.println("Enter 2 numbAs: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Enter float numba: ");
+        float c = sc.nextFloat();
+        System.out.println("Y0ur 1st numba is " + a);
+        System.out.println("Y0ur sec0nd numba is " + b);
+        System.out.println("Y0ur float numba is " + c);
+    }
+}
